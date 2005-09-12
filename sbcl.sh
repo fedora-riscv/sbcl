d7e1b50e11d03b80c4d7e362a4a9e9bf93d2ec73
Rex Dieter <rdieter@fedoraproject.org>
2005-09-12 12:39:38 +0000

    BCL_SETARCH -> SBCL_SETARCH (typo) support global /etc/sysconfig/sbcl     config

--- sbcl.sh
+++ sbcl.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+# Source global config, if it exists
+if [ -f /etc/sysconfig/sbcl ]; then
+  source /etc/sysconfig/sbcl
+fi
+
 if [ -z "$SBCL_HOME" ] ; then
 SBCL_HOME=/usr/lib/sbcl
 export SBCL_HOME
 fi
 
 if [ -z "$SBCL_SETARCH" ] ; then
-BCL_SETARCH="setarch i386 -R"
+SBCL_SETARCH="setarch i386 -R"
 fi
 
 exec ${SBCL_SETARCH} ${SBCL_HOME}/sbcl ${1+"$@"}
