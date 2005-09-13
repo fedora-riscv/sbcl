43c52d0faef00a66ff93585d45c6195f49cd5892
Rex Dieter <rdieter@fedoraproject.org>
2005-09-13 16:50:44 +0000

    - fix botched NO_ADDR_RANDOMIZE patch - fix botched LIB_DIR patch

--- sbcl.sh
+++ sbcl.sh
@@ -5,13 +5,4 @@ if [ -f /etc/sysconfig/sbcl ]; then
   source /etc/sysconfig/sbcl
 fi
 
-if [ -z "$SBCL_HOME" ] ; then
-SBCL_HOME=/usr/lib/sbcl
-export SBCL_HOME
-fi
-
-if [ -z "$SBCL_SETARCH" ] ; then
-SBCL_SETARCH="setarch i386 -R"
-fi
-
-exec ${SBCL_SETARCH} ${SBCL_HOME}/sbcl ${1+"$@"}
+exec ${SBCL_HOME}/sbcl ${1+"$@"}
